95a1fdae11e3f246b7f2b8b0bdd4cdec77015096
Or Zino <orzino3@gmail.com>
2025-01-03 19:58:36 +0200

    update utils.js

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,21 +1,7 @@
 export const formatProductName = (name) => {
   let formattedName = name
-    .replace(/[.#$[\]]/g, "_")
+    .replace(/[.#$[\]\/\\]/g, "_") // Added \/ to match / and \
     .replace(/\s+/g, "-")
     .replace(/^[-_]+|[-_]+$/g, "");
-
-  const hash = hashString(formattedName);
-
-  return hash;
-};
-
-export const hashString = (str) => {
-  let hash = 0;
-  if (str.length === 0) return hash;
-  for (let i = 0; i < str.length; i++) {
-    const char = str.charCodeAt(i);
-    hash = (hash << 5) - hash + char;
-    hash |= 0;
-  }
-  return hash.toString(36);
+  return formattedName;
 };
